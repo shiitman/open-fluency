03ece2432f5942415928577e169e59485b2f9bd9
Alexandr Wolodarskij <alex1d08@gmail.com>
2017-11-15 21:29:07 +0100

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,44 @@
+var drawGrid = new Draw(document.getElementById("canvas"));
+var micro = new Microphone();
+var fluency = new Fluency(drawGrid, micro);
+
+function initInput(id, func){
+  $("#"+id).keyup(function(){
+    $("#"+id).change();
+  });
+  $("#"+id).change(function(){
+    $("#"+id).val(fluency[func]($("#"+id).val()));    
+  });
+}
+
+$("#hear").click (function() {
+  $(this).text( (fluency.hear) ? "Start" : "Stop" );
+  $(this).toggleClass("btn-success");
+  $(this).toggleClass("btn-danger");
+  fluency.startHear();
+});
+
+$("#equalizer").change (function() {
+  fluency.equalizer=$("#equalizer").prop("checked");
+});
+
+$("#settingsButtons").click (function() {
+    $("#settings").toggle();
+});
+
+initInput("middlevolume", "setMiddleVolume");
+initInput("lowVolume", "setLowVolume");
+initInput("indent", "setIndent");
+initInput("pause", "setPauseTime");
+initInput("intro", "setIntro");
+initInput("smoothing", "setSmoothing");
+
+
+$("#settings").hide();
+
+fluency["setMiddleVolume"]("50");
+$("#middlevolume").val(fluency.middleVolume);
+
+if (!micro.hasGetUserMedia()) {
+  alert('getUserMedia() is not supported in your browser');
+}
